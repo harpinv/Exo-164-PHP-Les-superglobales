6456c8c9445b9a4fb9ad9cca270bdc472bce2940
vince <vincent.harpin@orange.fr>
2023-01-18 10:05:44 +0100

    exercice 164

--- index.php
+++ index.php
@@ -7,3 +7,10 @@
  * 3. Uploadez ce projet sur votre serveur et testez !
  */
 // TODO Votre code ici
+echo $_SERVER['PHP_SELF'];
+echo $_SERVER['SERVER_NAME'] . "<br>";
+echo $_SERVER['SERVER_ADDR'] . "<br>";
+echo $_SERVER['REMOTE_ADDR'] . "<br>";
+echo $_SERVER['HTTP_ACCEPT'] . "<br>";
+echo $_SERVER['HTTP_HOST'] . "<br>";
+echo $_SERVER['REQUEST_TIME'];
